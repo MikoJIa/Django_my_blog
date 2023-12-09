1f0ae58c1822fd6d64d013abceca64cb29fbd864
MikoJIa <Kolya_popov_86@tut.by>
2023-12-09 15:57:25 +0300

    create pagination.html and add functional post_list

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -22,7 +23,7 @@ class Post(models.Model):
 
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')  # поле слаг будет уникальным для даты publish
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -44,4 +45,10 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
+    # Функция reverse() будет формировать URL-адрес динамически, применяя
+    # имя URL-адреса, определенное в шаблонах URL-адресов.
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
